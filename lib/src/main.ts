e52d5d454693db72090b7b5524f91ce0939c7897
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-08-16 18:21:05 +0200

    refactor: split main function into subfunctions

--- lib/src/main.ts
+++ lib/src/main.ts
@@ -2,14 +2,15 @@ import { xml2json } from 'xml-js'
 import 'isomorphic-fetch'
 
 export class Options {
-    encoding: BufferEncoding | null
+    readonly encoding: BufferEncoding | null
 
     constructor(encoding: BufferEncoding | null = null) {
         this.encoding = encoding
     }
 }
 
-export function sitemap2urllist(data: string | Buffer, options: Options | null = null): string {
+function getSitemapContent(data: string | Buffer, options: Options | null): string {
+    // TODO: accept URL?
     let xml: string
     if (Buffer.isBuffer(data)) {
         xml = data.toString(options?.encoding ?? 'utf-8')
@@ -18,17 +19,60 @@ export function sitemap2urllist(data: string | Buffer, options: Options | null =
     } else {
         throw `Unknown input "${data}" of type ${typeof data}`
     }
+    return xml
+}
 
+class SitemapEntry {
+    readonly url: string
+    readonly altUrls: string[]
+    readonly priority: number
+
+    constructor(url: string, altUrls: string[], priority: number) {
+        this.url = url
+        this.altUrls = altUrls
+        this.priority = priority
+    }
+}
+
+function parseSingleEntry(entry: any): SitemapEntry {
+    let loc: string
+    try {
+        loc = entry['loc']['_text']
+    } catch (err) {
+        loc = ''
+    }
+
+    let priority: string
+    try {
+        priority = entry['priority']['_text']
+    } catch (err) {
+        priority = '0.5'
+    }
+
+    return new SitemapEntry(loc, [], parseFloat(priority))
+}
+
+function parseSitemapToEntries(xml: string): SitemapEntry[] {
     const json = JSON.parse(xml2json(xml, { compact: true, spaces: 0 }))
     const urls = json['urlset']['url']
-    let output: string[]
+    let output: SitemapEntry[] = []
 
     if (Array.isArray(urls)) {
-        output = urls.map(url => url['loc']['_text'])
+        output = urls.map(url => parseSingleEntry(url))
     } else if (urls) {
-        output = [urls['loc']['_text']]
-    } else {
-        output = []
+        output.push(parseSingleEntry(urls))
     }
-    return output.join('\n') + (output.length > 0 ? '\n' : '')
+    return output
+}
+
+function convertEntriesToUrls(entries: SitemapEntry[]): string[] {
+    return entries.map(entry => entry.url)
+}
+
+export function sitemap2urllist(data: string | Buffer, options: Options | null = null): string {
+    const xml = getSitemapContent(data, options)
+    const entries = parseSitemapToEntries(xml)
+    const urls = convertEntriesToUrls(entries)
+    // TODO: benchmark on huge sitemaps (depending on results consider streams or promises)
+    return urls.join('\n') + (urls.length > 0 ? '\n' : '')
 }
