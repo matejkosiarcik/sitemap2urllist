4a319585b0144243878fe5ae7c5fc82e6f28d622
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-03-31 20:49:01 +0200

    style: format js with eslint

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,127 +1,127 @@
-import { sitemap2urllist } from '../src/lib/lib';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as url from 'url';
 import { URL } from 'url';
+import { sitemap2urllist } from '../src/lib/lib';
 
 const projectPath = path.join(__dirname, '..');
 
 describe('Basic tests', () => {
-    const inputFilePath = path.join(projectPath, 'sitemaps', 'single-in.xml');
-    const outputFilePath = path.join(projectPath, 'sitemaps', 'single-out.txt');
-    const referenceOutput = fs.readFileSync(outputFilePath, 'utf-8');
+  const inputFilePath = path.join(projectPath, 'sitemaps', 'single-in.xml');
+  const outputFilePath = path.join(projectPath, 'sitemaps', 'single-out.txt');
+  const referenceOutput = fs.readFileSync(outputFilePath, 'utf-8');
 
-    test('Raw content as String', async () => {
-        // given
-        const input: string = fs.readFileSync(inputFilePath, 'utf-8');
+  test('Raw content as String', async () => {
+    // given
+    const input: string = fs.readFileSync(inputFilePath, 'utf-8');
 
-        // when
-        const testedOutput = await sitemap2urllist(input);
+    // when
+    const testedOutput = await sitemap2urllist(input);
 
-        // then
-        expect(testedOutput).toBe(referenceOutput);
-    });
+    // then
+    expect(testedOutput).toBe(referenceOutput);
+  });
 
-    test('Raw content as Buffer', async () => {
-        // given
-        const input: Buffer = fs.readFileSync(inputFilePath);
+  test('Raw content as Buffer', async () => {
+    // given
+    const input: Buffer = fs.readFileSync(inputFilePath);
 
-        // when
-        const testedOutput = await sitemap2urllist(input);
+    // when
+    const testedOutput = await sitemap2urllist(input);
 
-        // then
-        expect(testedOutput).toBe(referenceOutput);
-    });
+    // then
+    expect(testedOutput).toBe(referenceOutput);
+  });
 
-    test('Empty string', async () => {
-        try {
-            const result = await sitemap2urllist('');
-            fail(`Should fail, instead got ${result}`);
-        } catch (error) {
-            // success
-        }
-    });
+  test('Empty string', async () => {
+    try {
+      const result = await sitemap2urllist('');
+      fail(`Should fail, instead got ${result}`);
+    } catch (error) {
+      // success
+    }
+  });
 
-    test('Filepath - 1', async () => {
-        // given
-        const inputUrl = url.pathToFileURL(inputFilePath);
-        expect(inputUrl.protocol).toBe('file:');
+  test('Filepath - 1', async () => {
+    // given
+    const inputUrl = url.pathToFileURL(inputFilePath);
+    expect(inputUrl.protocol).toBe('file:');
 
-        // when
-        const testedOutput = await sitemap2urllist(inputUrl);
+    // when
+    const testedOutput = await sitemap2urllist(inputUrl);
 
-        // then
-        expect(testedOutput).toBe(referenceOutput);
-    });
+    // then
+    expect(testedOutput).toBe(referenceOutput);
+  });
 
-    test('Filepath - 2', async () => {
-        // given
-        const inputUrl = new URL(`file://${inputFilePath}`);
+  test('Filepath - 2', async () => {
+    // given
+    const inputUrl = new URL(`file://${inputFilePath}`);
 
-        // when
-        const testedOutput = await sitemap2urllist(inputUrl);
+    // when
+    const testedOutput = await sitemap2urllist(inputUrl);
 
-        // then
-        expect(testedOutput).toBe(referenceOutput);
-    });
+    // then
+    expect(testedOutput).toBe(referenceOutput);
+  });
 
-    test('Filepath - nonexistent', async () => {
-        // given
-        const inputUrl = new URL(`file:///nonexistent.xml`);
+  test('Filepath - nonexistent', async () => {
+    // given
+    const inputUrl = new URL('file:///nonexistent.xml');
 
-        try {
-            const result = await sitemap2urllist(inputUrl);
-            fail(`Should fail, instead got ${result}`);
-        } catch (error) {
-            // success
-        }
-    });
+    try {
+      const result = await sitemap2urllist(inputUrl);
+      fail(`Should fail, instead got ${result}`);
+    } catch (error) {
+      // success
+    }
+  });
 
-    test('Remote sitemap - https', async () => {
-        // given
-        const inputUrl = new URL(`https://www.sitemaps.org/sitemap.xml`);
+  test('Remote sitemap - https', async () => {
+    // given
+    const inputUrl = new URL('https://www.sitemaps.org/sitemap.xml');
 
-        // when
-        const testedOutput = await sitemap2urllist(inputUrl);
+    // when
+    const testedOutput = await sitemap2urllist(inputUrl);
 
-        // then
-        expect(testedOutput.split('\n').length).toBeGreaterThanOrEqual(2);
-    });
+    // then
+    expect(testedOutput.split('\n').length).toBeGreaterThanOrEqual(2);
+  });
 
-    test('Remote sitemap - http', async () => {
-        // given
-        const inputUrl = new URL(`http://www.sitemaps.org/sitemap.xml`);
+  test('Remote sitemap - http', async () => {
+    // given
+    const inputUrl = new URL('http://www.sitemaps.org/sitemap.xml');
 
-        // when
-        const testedOutput = await sitemap2urllist(inputUrl);
+    // when
+    const testedOutput = await sitemap2urllist(inputUrl);
 
-        // then
-        expect(testedOutput.split('\n').length).toBeGreaterThanOrEqual(2);
-    });
+    // then
+    expect(testedOutput.split('\n').length).toBeGreaterThanOrEqual(2);
+  });
 
-    test('Remote sitemap 404 - https', async () => {
-        // given
-        const inputUrl = new URL(`https://www.sitemaps.org/sitemap-nonexistent.xml`);
+  test('Remote sitemap 404 - https', async () => {
+    // given
+    const inputUrl = new URL('https://www.sitemaps.org/sitemap-nonexistent.xml');
 
-        // when
-        try {
-            const result = await sitemap2urllist(inputUrl);
-            fail(`Should fail, instead got ${result}`);
-        } catch (error) {
-            // success
-        }
-    });
+    // when
+    try {
+      const result = await sitemap2urllist(inputUrl);
+      fail(`Should fail, instead got ${result}`);
+    } catch (error) {
+      // success
+    }
+  });
 
-    test('Remote sitemap 404 - http', async () => {
-        // given
-        const inputUrl = new URL(`http://www.sitemaps.org/sitemap-nonexistent.xml`);
+  test('Remote sitemap 404 - http', async () => {
+    // given
+    const inputUrl = new URL('http://www.sitemaps.org/sitemap-nonexistent.xml');
 
-        // when
-        try {
-            const result = await sitemap2urllist(inputUrl);
-            fail(`Should fail, instead got ${result}`);
-        } catch (error) {
-            // success
-        }
-    });
+    // when
+    try {
+      const result = await sitemap2urllist(inputUrl);
+      fail(`Should fail, instead got ${result}`);
+    } catch (error) {
+      // success
+    }
+  });
 });
