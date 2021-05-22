7c8e26ac900020daefbe3e7a329e4747f07b1126
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-05-22 18:31:10 +0200

    delete: old typescript code

--- bin/main.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-// This file exists because you can't put ^^shebang^^ on top of TypeScript file
-// So we make this plain JavaScript file instead, and call the TypeScript one
-require('../dist/cli/cli.js');
