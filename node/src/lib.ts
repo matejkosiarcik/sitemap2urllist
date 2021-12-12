daec146741d95d7faac34773435e240678aba462
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-12-12 20:06:04 +0100

    chore: update packages

--- node/src/lib.ts
+++ node/src/lib.ts
@@ -1,11 +1,11 @@
 import * as sitemap2urllist from '../wasm/sitemap2urllist';
 
-export async function convert(input: string, _alternate?: boolean): Promise<Array<string>> {
+export async function convert(input: string, _alternate?: boolean): Promise<string[]> {
   const alternate = _alternate ?? false;
   return await sitemap2urllist.convert(input, alternate);
 }
 
-export function save(output: string, urls: Array<string>): void {
+export function save(output: string, urls: string[]): void {
   return sitemap2urllist.save(output, urls);
 }
 
